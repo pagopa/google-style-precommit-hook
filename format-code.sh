be92ca3b151dda05fb027ee738509ebf68786bc1
Jacopo Carlini <jacopo.carlini@gmail.com>
2024-11-25 12:24:02 +0100

    Update format-code.sh

--- format-code.sh
+++ format-code.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/env sh
+
+# Directory cache
 mkdir -p .cache
 cd .cache
-if [ ! -f google-java-format-1.7-all-deps.jar ]
-then
-    curl -LJO "https://github.com/google/google-java-format/releases/download/google-java-format-1.7/google-java-format-1.7-all-deps.jar"
-    chmod 755 google-java-format-1.7-all-deps.jar
+
+# Scarica l'ultima versione del jar
+if [ ! -f google-java-format.jar ]; then
+    echo "Downloading the latest google-java-format jar..."
+    LATEST_RELEASE=$(curl -s https://api.github.com/repos/google/google-java-format/releases/latest | grep "tag_name" | cut -d '"' -f 4)
+    JAR_URL="https://github.com/google/google-java-format/releases/download/${LATEST_RELEASE}/google-java-format-${LATEST_RELEASE#v}-all-deps.jar"
+    curl -LJO "$JAR_URL"
+    mv "google-java-format-${LATEST_RELEASE#v}-all-deps.jar" google-java-format.jar
+    chmod 755 google-java-format.jar
 fi
 cd ..
 
-changed_java_files=$(git diff --cached --name-only --diff-filter=ACMR | grep ".*java$" )
+# Trova i file .java modificati
+changed_java_files=$(git diff --cached --name-only --diff-filter=ACMR | grep ".*java$")
 echo $changed_java_files
-java -jar .cache/google-java-format-1.7-all-deps.jar --replace $changed_java_files
+
+# Applica il formattatore ai file modificati
+if [ -n "$changed_java_files" ]; then
+    java -jar .cache/google-java-format.jar --replace $changed_java_files
+else
+    echo "No changed Java files to format."
+fi
